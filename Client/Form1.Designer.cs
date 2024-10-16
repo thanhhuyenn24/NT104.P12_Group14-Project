f1ee0f64f4d7d42caa19ad9f686b0b0d32b1239e
DoNgocThaoNguyen <23521038@gm.uit.edu.vn>
2024-10-16 14:29:37 +0700

    Add TextBox nhập tên người chơi

--- Client/Form1.Designer.cs
+++ Client/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.username = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button2
@@ -41,20 +43,41 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(340, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Bắt đầu";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(327, 108);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(100, 22);
+            this.username.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox username;
     }
 }
 
