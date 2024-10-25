9335ed64d40e86e49a2358beece7b754ef6700f4
DoNgocThaoNguyen <23521038@gm.uit.edu.vn>
2024-10-25 22:59:13 +0700

    Add Class: WordData để lấy random từ trong này.

---
+++ Server/WordData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public static class WordData
+    {
+        public static List<string> wordList = new List<string>()
+    {
+        "apple", "car", "tree", "house", "dog", "cat", "sun", "ball", "book", "plane"
+        // Thêm các từ khác
+    };
+    }
+}
