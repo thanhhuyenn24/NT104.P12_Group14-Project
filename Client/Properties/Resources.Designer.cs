9b5d0e6c1ba1847fdc415ac0d22e268be1a88937
Duong Thanh Huyen <23520659@gm.uit.edu.vn>
2024-10-27 00:30:35 +0700

    Add các tính năng để vẽ trong GiaoDienNguoiChoi

--- Client/Properties/Resources.Designer.cs
+++ Client/Properties/Resources.Designer.cs
@@ -59,5 +59,35 @@ namespace Client.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_eraser_tool_24 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-eraser-tool-24", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_paint_24 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-paint-24", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_sign_up_24 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-sign-up-24", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
